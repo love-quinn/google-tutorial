22c261f1340fd1aca9b8325ffe6b092640bbfd39
love-quinn <lucaslucal75@gmail.com>
2024-10-09 20:19:30 -0300

    chore: add api auth route

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,9 @@
-import NextAuth from "next-auth"
+import NextAuth, { AuthOptions } from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "./prisma"
 import GoogleProvider from "next-auth/providers/google"
  
-export const { handlers, auth, signIn, signOut } = NextAuth({
+export const authOptions: AuthOptions = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
